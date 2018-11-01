2f8766bc0f7cbad98ca4642b563ae0a96264e67d
sensible-proc-7557 <vchan313@gmail.com>
2018-11-01 15:42:14 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown (int)
+  counter = 0 
+  while counter > 0 
+    puts "#{int} SECOND(S)!"
+    counter -= 1
+  end
+  puts "HAPPY NEW YEAR!"  
 end
